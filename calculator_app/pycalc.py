1c30391b1988b35810adde2de059f08ee5883290
Victoria Hodges <victoriaehodges@gmail.com>
2023-10-23 21:26:34 +0200

    add controller

--- calculator_app/pycalc.py
+++ calculator_app/pycalc.py
@@ -3,6 +3,7 @@
 """PyCalc is a simple calculator built with Python and PyQt."""
 
 import sys
+from functools import partial
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
@@ -82,11 +83,41 @@ def evaluateExpression(expression):
     return result
 
 
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
+
 def main():
     """PyCalc's main function."""
     pycalcApp = QApplication([])
     pycalcWindow = PyCalcWindow()
     pycalcWindow.show()
+    PyCalc(model=evaluateExpression, view=pycalcWindow)
     sys.exit(pycalcApp.exec())
 
 
