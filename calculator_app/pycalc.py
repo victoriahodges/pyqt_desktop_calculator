41dae3ed136bc756e2896749f1311c8502116b79
Victoria Hodges <victoriaehodges@gmail.com>
2023-10-25 20:30:14 +0200

    add more buttons, change appearance

--- calculator_app/pycalc.py
+++ calculator_app/pycalc.py
@@ -6,6 +6,7 @@ import sys
 from functools import partial
 
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeySequence
 from PyQt6.QtWidgets import (
     QApplication,
     QGridLayout,
@@ -15,26 +16,28 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget
 )
-from PyQt6.QtGui import QKeySequence
 
 ERROR_MSG = "ERROR"
-WINDOW_SIZE = 400
+WINDOW_SIZE_WIDTH = 392
+WINDOW_SIZE_HEIGHT = 500
 DISPLAY_HEIGHT = 60
 BUTTON_SIZE = 70
 KEYBOARD = [
-            ["7", "8", "9", "/", "C"],
-            ["4", "5", "6", "*", "("],
-            ["1", "2", "3", "-", ")"],
-            ["0", "00", ".", "+", "="],
-        ]
+    ["⌫", "(", ")", "π", "C"],
+    ["7", "8", "9", "/", "√"],
+    ["4", "5", "6", "*", "%"],
+    ["1", "2", "3", "-", "="],
+    ["0", "00", ".", "+"],
+]
 
 
 class PyCalcWindow(QMainWindow):
     """PyCalc's main window (GUI or view)."""
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyCalc")
-        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.setFixedSize(WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT)
         self.generalLayout = QVBoxLayout()
         centralWidget = QWidget(self)
         centralWidget.setLayout(self.generalLayout)
@@ -56,10 +59,16 @@ class PyCalcWindow(QMainWindow):
             for col, key in enumerate(keys):
                 self.buttonMap[key] = QPushButton(key)
                 self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                self.buttonMap[key].setShortcut(QKeySequence(key))
+                if key.isnumeric():
+                    self.buttonMap[key].setStyleSheet("background-color: lightgray")
                 if key == "=":
                     self.buttonMap[key].setStyleSheet("background-color: orange")
-                self.buttonMap[key].setShortcut(QKeySequence(key))
-                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+                    self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE * 2 + 8)
+                    buttonsLayout.addWidget(self.buttonMap[key], row, col, 2, 1)
+
+                else:
+                    buttonsLayout.addWidget(self.buttonMap[key], row, col)
 
         self.generalLayout.addLayout(buttonsLayout)
 
@@ -107,9 +116,7 @@ class PyCalc:
     def _connectSignalsAndSlots(self):
         for keySymbol, button in self._view.buttonMap.items():
             if keySymbol not in {"=", "C"}:
-                button.clicked.connect(
-                    partial(self._buildExpression, keySymbol)
-                )
+                button.clicked.connect(partial(self._buildExpression, keySymbol))
         self._view.buttonMap["="].clicked.connect(self._calculateResult)
         self._view.display.returnPressed.connect(self._calculateResult)
         self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
